053390ec16187bfa76ef6edad4cc2563a7eb031a
Xyphuz <hansen0715.2@gmail.com>
2024-04-30 20:22:54 +0800

    chore: set engine as async

--- src/commands/run.rs
+++ src/commands/run.rs
@@ -110,10 +110,11 @@ enum CliLinker {
 
 impl RunCommand {
     /// Executes the command.
-    pub fn execute(mut self) -> Result<()> {
+    pub async fn execute(mut self) -> Result<()> {
         self.run.common.init_logging()?;
 
         let mut config = self.run.common.config(None)?;
+        config.async_support(true);
 
         if self.run.common.wasm.timeout.is_some() {
             config.epoch_interruption(true);
